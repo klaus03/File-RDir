fde9b7b5dfeff685b1777ba95fa6fd1be33b97ee
Klaus Eichner <klaus03@gmail.com>
2015-11-27 20:28:26 +0100

    Initial commit

---
+++ lib/File/RDir.pm
@@ -0,0 +1,103 @@
+package File::RDir;
+
+use strict;
+use warnings;
+
+use Carp qw(croak);
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+our %EXPORT_TAGS = ( 'all' => [ qw(read_rdir) ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+
+sub new {
+    my $pkg = shift;
+    my ($dir) = @_;
+    $dir =~ s{\\}'/'xmsg;
+
+    opendir my $hdl, $dir or croak "Can't opendir '$dir' because $!";
+
+    my $self = { 'root' => $dir, 'ndir' => '', 'dlist' => [], 'hdl' => $hdl };
+
+    bless $self, $pkg;
+}
+
+sub match {
+    my $self = shift;
+    return unless $self->{'hdl'};
+
+    my $ele;
+    my $full_dir = $self->{'root'}.$self->{'ndir'};
+
+    LOOP1: {
+        $ele = readdir $self->{'hdl'};
+
+        unless (defined $ele) {
+            closedir $self->{'hdl'};
+            $self->{'hdl'} = undef;
+
+            my $ndir = shift @{$self->{'dlist'}};
+            last LOOP1 unless defined $ndir;
+
+            $self->{'ndir'} = $ndir;
+
+            $full_dir = $self->{'root'}.$self->{'ndir'};
+            opendir $self->{'hdl'}, $full_dir or croak "Can't opendir '$full_dir' because $!";
+            redo LOOP1;
+        }
+
+        redo LOOP1 if $ele eq '.' or $ele eq '..'; # <-- This is highly important !!!
+
+        my $full_ele = $full_dir.'/'.$ele;
+
+        if (-d $full_ele) {
+            push @{$self->{'dlist'}}, $self->{'ndir'}.'/'.$ele;
+            redo LOOP1;
+        }
+    }
+
+    return unless defined $ele;
+
+    return $self->{'ndir'}.'/'.$ele;
+}
+
+sub read_rdir {
+    my ($dir) = @_;
+
+    my @FList;
+
+    my $obj = File::RDir->new($dir);
+
+    while (defined(my $file = $obj->match)) {
+        push @FList, $file;
+    }
+
+    return @FList;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+File::RDir - List directories and recurse into subdirectories.
+
+=head1 SYNOPSIS
+
+  use File::RDir;
+
+=head1 AUTHOR
+
+Klaus Eichner, November 2015
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by Klaus Eichner
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/0010_test.t
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+use Test::More tests => 2;
+
+use_ok('File::RDir');
+
+my $tdir = 'testdir';
+
+is_deeply( [ sort(File::RDir::read_rdir($tdir)) ],
+           [ '/File01.txt',
+             '/Sub01/AA_hidden/File07.txt',
+             '/Sub01/AA_hidden/Zzz/File08.txt',
+             '/Sub01/D01/File04.txt',
+             '/Sub01/D02/File05.txt',
+             '/Sub01/File02.txt',
+             '/Sub01/File03.txt',
+             '/Sub02/D03/File06.txt' ],
+           "First simple test" );
